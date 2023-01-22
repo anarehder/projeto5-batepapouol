57387b8a3ddcebd908abb82510dee525ac4626aa
anarehder <anapaula.rehder@gmail.com>
2023-01-22 16:58:02 -0300

    Tela Entrada

--- scripts.js
+++ scripts.js
@@ -1,18 +1,10 @@
-/// <div data-test="message" class="mensagem entrou">
-// <span class="hora">(${chat[i].time}) </span><span class="nome"> ${chat[i].from} </span>${chat[i].text}
-// </div>`
-
 let meuNome;
 let mensagens, mensagensFiltradas;
 let chat = document.querySelector(".main");
+let telaEntrada = document.querySelector(".telaEntrada");
+let telaChat = document.querySelector(".fundo");
 
 chat.innerHTML = "";
-
-perguntarNome(); // já busco as mensagens imediatamente se a resposta for ok!
-
-setInterval(manterConexao, 5000);
-
-setInterval(buscarMensagem, 3000);
 
 function perguntarNome(){
     meuNome = prompt("Qual seu nome?");
@@ -28,6 +20,36 @@ function perguntarNome(){
         location.reload();
        }
     });
+}
+
+function entrarSala(){
+    meuNome = document. querySelector(".insereNome"). value;
+    if (meuNome === ""){
+        location.reload();
+    }
+
+    telaEntrada.innerHTML = `<img class="logoEntrada" src="./imagens/logo 2.png" alt="logo">;
+        <img class="carregando" src="./imagens/carregando.gif" alt="carregando">
+        <p class="entrando">Entrando...</p>`;
+
+    const usuario = { name: meuNome }; //rótulo e variável
+
+    const promessaUsuario = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",usuario);
+
+    promessaUsuario.then(response => {buscarMensagem()
+        setInterval(buscarMensagem, 3000);
+        setInterval(manterConexao, 5000);
+        telaEntrada.classList.add("escondido");
+        telaChat.classList.remove("escondido");
+    });
+
+    promessaUsuario.catch(erro => {if (erro.request.status === 400){
+        alert("Este usuário já está sendo utilizado");
+        location.reload();
+       }
+    });
+
+
 }
 
 function manterConexao(){
@@ -100,16 +122,17 @@ function exibirPorUltimo(){
 }
 
 function enviarMensagem(){
-    const texto = document. querySelector("input"). value;
+    const texto = document. querySelector(".insereMensagem"). value;
 
-    const mensagemNova = { from: meuNome, to: "Todos", text: texto, type: "message" }
+    const mensagemNova = { from: meuNome, to: "Todos", text: texto, type: "message" };
     // ou usuario selecionado para o bônus ou "private_message" para o bônus
     console. log(texto);
     console. log(mensagemNova);
 
     const promessaEnvioMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",mensagemNova);
 
-    promessaEnvioMensagem.then(response => buscarMensagem());
+    promessaEnvioMensagem.then(response => {buscarMensagem();
+        document. querySelector(".insereMensagem"). value="";});
 
     promessaEnvioMensagem.catch(erro => location.reload());
 }
